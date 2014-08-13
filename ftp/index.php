dc12ea56502b62ecb508e9a1f0359f554e1e3c0b
tomdiben <tomdiben@msn.com>
2014-08-13 10:34:29 -0300

    Creation2
    Restes de ce que j'avais commencé

---
+++ ftp/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html> 
+<html lang="en-US">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<title>A rua digital | Seu caminho passa pela rede</title>
+	<meta name"robots" content="noindex, nofollow">
+
+	<link rel="stylesheet" href="css/style.css">
+	
+	<!--
+	<link rel="stylesheet" media=" (max-width: 800px) " href="css/small.css">
+	<link rel="stylesheet" media=" (min-width: 801px) " href="css/big.css">
+	-->
+
+</head>
+
+<body>
+	<div id="page">
+	
+		<div class="main">
+			
+			<div class="header border">
+				<img src="images/logov2.fw.png" class="logo border block"/>
+			</div>
+			
+			<div class="clear"></div>
+
+			<div class="content border">
+				content
+			</div>
+
+			<div class="footer border">
+				footer
+			</div>
+
+		</div>
+
+	</div>
+
+</body>
